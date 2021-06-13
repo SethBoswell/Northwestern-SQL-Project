a713826b52251a39ad7420947f764c5240b8557d
SethBoswell <seth.stanton.boswell@gmail.com>
2021-06-13 15:16:09 -0500

    adding challenge

--- Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -23,3 +23,17 @@ SELECT COUNT(u.emp_no), u.title
 FROM unique_titles as u
 GROUP BY u.title
 ORDER BY COUNT(u.emp_no) DESC;
+
+-- Mentorship Eligibility Table
+SELECT DISTINCT ON (e.emp_no), e.first_name, e.last_name, e.birth_date,
+        de.from_date, de.to_date,
+        t.title
+-- INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01');
+ORDER BY e.emp_no;
